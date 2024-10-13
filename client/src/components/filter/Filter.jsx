19639184081560a675c7ac51d5d330727ca74110
Rashi <rashi1704baranwal@gmail.com>
2024-10-13 14:28:39 +0530

    feat:

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -2,27 +2,27 @@ import { useState } from "react";
 import "./filter.scss";
 import { useSearchParams } from "react-router-dom";
 
-function Filter() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [query, setQuery] = useState({
-    type: searchParams.get("type") || "",
-    city: searchParams.get("city") || "",
-    property: searchParams.get("property") || "",
-    minPrice: searchParams.get("minPrice") || "",
-    maxPrice: searchParams.get("maxPrice") || "",
-    bedroom: searchParams.get("bedroom") || "",
-  });
-
-  const handleChange = (e) => {
-    setQuery({
-      ...query,
-      [e.target.name]: e.target.value,
+  function Filter() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const [query, setQuery] = useState({
+      type: searchParams.get("type") || "",
+      city: searchParams.get("city") || "",
+      property: searchParams.get("property") || "",
+      minPrice: searchParams.get("minPrice") || "",
+      maxPrice: searchParams.get("maxPrice") || "",
+      bedroom: searchParams.get("bedroom") || "",
     });
-  };
 
-  const handleFilter = () => {
-    setSearchParams(query);
-  };
+    const handleChange = (e) => {
+      setQuery({
+        ...query,
+        [e.target.name]: e.target.value,
+      });
+    };
+
+    const handleFilter = () => {
+      setSearchParams(query);
+    };
 
   return (
     <div className="filter">
